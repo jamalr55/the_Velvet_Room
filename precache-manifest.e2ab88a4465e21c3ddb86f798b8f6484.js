fd36b880fc8c57b2d4ed6d08ecf8e29ad94ca265
Jamal Richardson <jmlrchrdsn55@gmail.com>
2019-05-20 15:08:30 -0500

    Updates

---
+++ precache-manifest.e2ab88a4465e21c3ddb86f798b8f6484.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc2db260496dcba223a2195c57788606",
+    "url": "/the_Velvet_Room/index.html"
+  },
+  {
+    "revision": "d56def3216b5c3656e11",
+    "url": "/the_Velvet_Room/static/css/main.449dab84.chunk.css"
+  },
+  {
+    "revision": "e1692d64b4a74c29c9a4",
+    "url": "/the_Velvet_Room/static/js/2.c4fa2b25.chunk.js"
+  },
+  {
+    "revision": "d56def3216b5c3656e11",
+    "url": "/the_Velvet_Room/static/js/main.7c88fafb.chunk.js"
+  },
+  {
+    "revision": "caf8bcdf188b78b7fc6f",
+    "url": "/the_Velvet_Room/static/js/runtime~main.de712a16.js"
+  },
+  {
+    "revision": "8f202ee5810fbf3f6b987b52ea13add8",
+    "url": "/the_Velvet_Room/static/media/slide_4.8f202ee5.jpg"
+  },
+  {
+    "revision": "d35069b0bbba40bbb5808ba00df5a028",
+    "url": "/the_Velvet_Room/static/media/slide_5.d35069b0.jpg"
+  },
+  {
+    "revision": "be80c4d220c456e49eb4e028b7d6d2d9",
+    "url": "/the_Velvet_Room/static/media/slide_6.be80c4d2.jpg"
+  }
+]);
